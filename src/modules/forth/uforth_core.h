935e735b6d62101988a0d750ec82a373cad04037
fxiii <fxiii@gitlab.com>
2026-08-12 01:02:56 +0200

    chore(forth): add missing uforth_core.h header dependency

---
+++ src/modules/forth/uforth_core.h
@@ -0,0 +1,14 @@
+#ifndef UFORTH_CORE_H
+#define UFORTH_CORE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uforth_load_core(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UFORTH_CORE_H */
